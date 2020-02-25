58563e07a77f99c4243eb5da37082a49dd4b938a
Brus Palaj <brus.palaj71@gmail.com>
2020-02-25 11:26:32 -0500

    updated readme file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,20 @@
-//Get the generation button id and create a variable for the password
+// Assignment code 
 var generateBtn = document.querySelector("#generate");
 var generatedPassword = "";
 
-//Store the character information in seperate variables
 var lowerCaseLetterCharacters = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseLetterCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberCharacters = "0123456789";
 var specialCharacters = "!@#$%^&&*()";
 var validCharacters = "";
 
-//Check character inputs
+//creating variables for character inputs
 var generateLowerCase = document.getElementById("inputLowerCase");
 var generateUpperCase = document.getElementById("inputUpperCase");
 var generateNumbers = document.getElementById("inputNumbers");
 var generateSpecials = document.getElementById("inputSpecials");
 
-//Check if an input for a character is pressed and if it is add to the validCharacters variable
+//If an input for a character is pressed, add to the validCharacters variable
 function generateValidCharacters() {
     if(generateLowerCase.checked) {
         validCharacters += lowerCaseLetterCharacters;
@@ -31,26 +30,26 @@ function generateValidCharacters() {
     }
 }
 
+//Function that will return randomized characters (either number or strings) which is converted using the ParseInt method 
 function getRandomFromArray(arr) {
     return arr[parseInt(Math.random() * arr.length)];
 }
 
+// 
 function addCharFromArray(amountOfChars) {
     for (var i = 0; i < amountOfChars; i++) {
         generatedPassword += getRandomFromArray(validCharacters);
     }
 }
 
+// Wrote function that will generate password based on what is clicked 
 function generatePassword(length) {
-    // var passwordLength = document.getElementById("myPasswordLength").value;
     if(generateLowerCase.checked || generateUpperCase.checked || generateNumbers.checked || generateSpecials) {
         generateValidCharacters();
         validCharacters.split();
         addCharFromArray(length);
         return generatedPassword;
-    } else {
-        alert("Please enter at least 1 character.");
-    }
+    } 
 }
 
 // Write password to the #password input
@@ -60,9 +59,7 @@ function writePassword() {
     var passwordLength = document.getElementById("myPasswordLength").value;
     parseInt(passwordLength);
 
-    //Check for character specification inputs
-
-
+    //Check if for user inputs is between 8 and 128 characters, alert user to enter number if no number is entered 
     if(passwordLength >= 8 && passwordLength <= 128) {
         var password = generatePassword(passwordLength);
         var passwordText = document.querySelector("#password");
---
+++ README.md
@@ -0,0 +1,10 @@
+# 03 JavaScript: Password Generator
+
+* Started this off by creating variables for all characters that will be used to formulate "secure password" (Upper/Lower case, numbers and special characters).
+* Next, I created different inputs on my HTML to specify which options should be excercised in the password and followed up by creating variables for character inputs to be changes upon selection 
+* Then, created an IF statement that will add each different option to my validcharacters variable if checked on the input checkbox 
+* Created Function that will return randomized characters (either number or strings) which is converted using the ParseInt method 
+*
+* Created a function that will generate a password based on IF/WHICH one of the option are checked off in the input boxes
+* Function that will make a password and add to the password input with an IF statement that makes sure user enters a number between 8-128 deciding before getting a generated password 
+* Finally an event listener that calls the writePassword function when the generateBTN is clicked. 
