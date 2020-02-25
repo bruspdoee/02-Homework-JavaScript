cac4b1a76b8dbce87d4e3e63d765ce0bf43509bf
Brus Palaj <brus.palaj71@gmail.com>
2020-02-24 20:49:03 -0500

    made changes to js file, adding all logic for the password gen to function, edited HTML to add on clickables to specifiy password details - if you want to include special characters, upper/lower case, etc

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,76 @@
-// Assignment Code
+  
+//Get the generation button id and create a variable for the password
 var generateBtn = document.querySelector("#generate");
+var generatedPassword = "";
+
+//Store the character information in seperate variables
+var lowerCaseLetterCharacters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetterCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberCharacters = "0123456789";
+var specialCharacters = "!@#$%^&&*()";
+var validCharacters = "";
+
+//Check character inputs
+var generateLowerCase = document.getElementById("inputLowerCase");
+var generateUpperCase = document.getElementById("inputUpperCase");
+var generateNumbers = document.getElementById("inputNumbers");
+var generateSpecials = document.getElementById("inputSpecials");
+
+//Check if an input for a character is pressed and if it is add to the validCharacters variable
+function generateValidCharacters() {
+    if(generateLowerCase.checked) {
+        validCharacters += lowerCaseLetterCharacters;
+    }
+    if (generateUpperCase.checked) {
+        validCharacters += upperCaseLetterCharacters;
+    }  
+    if (generateNumbers.checked) {
+        validCharacters += numberCharacters;
+    }  
+    if (generateSpecials.checked) {
+        validCharacters += specialCharacters;
+    }
+}
+
+function getRandomFromArray(arr) {
+    return arr[parseInt(Math.random() * arr.length)];
+}
+
+function addCharFromArray(amountOfChars) {
+    for (var i = 0; i < amountOfChars; i++) {
+        generatedPassword += getRandomFromArray(validCharacters);
+    }
+}
+
+function generatePassword(length) {
+    // var passwordLength = document.getElementById("myPasswordLength").value;
+    if(generateLowerCase.checked || generateUpperCase.checked || generateNumbers.checked || generateSpecials) {
+        generateValidCharacters();
+        validCharacters.split();
+        addCharFromArray(length);
+        return generatedPassword;
+    } else {
+        alert("Please enter at least 1 character.");
+    }
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    generatedPassword = "";
+    validCharacters = "";
+    var passwordLength = document.getElementById("myPasswordLength").value;
+    parseInt(passwordLength);
 
-  passwordText.value = password;
+    //Check for character specification inputs
 
+
+    if(passwordLength >= 8 && passwordLength <= 128) {
+        var password = generatePassword(passwordLength);
+        var passwordText = document.querySelector("#password");
+        passwordText.value = password;
+    } else {
+        alert("Please enter a value between 8 and 128");
+    }
 }
 
 // Add event listener to generate button
